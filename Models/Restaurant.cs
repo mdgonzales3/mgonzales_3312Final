a51ba7636d2eecd0bac59cf8e837e9ffd4b8d860
Matthew Gonzales <mgonzales3@buffs.wtamu.edu>
2022-12-06 10:08:27 -0600

    3nd commit

---
+++ Models/Restaurant.cs
@@ -0,0 +1,13 @@
+namespace RestaurantASP.Models
+{
+    public class Restaurant
+    {
+        public int RestaurantID {get; set;}
+
+        public string RestaurantName {get; set;} = string.Empty;
+
+        public string Location {get; set;} = string.Empty;
+    }
+    
+}
+
---
+++ Models/Reviews.cs
@@ -0,0 +1,15 @@
+namespace RestaurantASP.Models
+{
+    public class Review
+    {
+        public int ReviewID {get; set;}
+
+        public decimal Price {get; set;}
+
+        public bool Tossed {get; set;}
+
+        public decimal Rating {get; set;}
+    }
+    
+}
+
